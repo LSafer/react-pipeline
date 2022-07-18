74ed9ae61103c5ed243ab8cf9d6f50219b29744b
Sulaiman Oboody <lsafer@cufy.org>
2022-07-18 19:41:45 +0300

    refactor: Reformat Code, Documentation, General Optimization

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,83 @@
 import React, {createContext, ReactNode, useContext} from "react";
 
-const PipeContext = createContext<ReactNode>(<></>)
+/**
+ * The context holding the component to be rendered on {@link Pipe}.
+ */
+const PipeContext = createContext<ReactNode>(<></>);
 
+/**
+ * A provider providing the component to be rendered on {@link Pipe}.
+ *
+ * @since 1.0.0
+ */
 export function PipeProvider(props: {
     component: ReactNode,
     children: ReactNode
 }) {
     return <PipeContext.Provider value={props.component}>
         {props.children}
-    </PipeContext.Provider>
+    </PipeContext.Provider>;
 }
 
+/**
+ * Return the component to be rendered on {@link Pipe}.
+ *
+ * @since 1.0.0
+ */
 export function usePipe() {
-    return useContext(PipeContext)
+    return useContext(PipeContext);
 }
 
-export function Pipe(props: {}) {
-    const pipe = usePipe()
+/**
+ * Render the next component in the pipeline.
+ *
+ * @since 1.0.0
+ */
+export function Pipe() {
+    const pipe = usePipe();
 
-    return <>{pipe}</>
+    return <>{pipe}</>;
 }
 
+/**
+ * Render the provided components with the last
+ * component's pipe being the current pipe.
+ *
+ * Note: this component will add the current pipe
+ * to the components list, enabling the last given
+ * component to render the next component in the
+ * parent pipeline.
+ *
+ * @since 1.0.0
+ */
 export function Pipeline(props: {
     components: ReactNode[]
 }) {
-    const pipe = usePipe()
-    const [component, ...components] = props.components
+    const pipe = usePipe();
+    const [component, ...components] = props.components;
 
     return <PipeProvider
         component={<Piping components={[...components, pipe]} />}
         children={component}
-    />
+    />;
 }
 
+/**
+ * Render the provided components.
+ *
+ * Note: this component won't pass the current pipe.
+ * If this was in another piping, The `<Pipe />` component
+ * in this piping won't render the next component in the parent piping.
+ *
+ * @since 1.0.0
+ */
 export function Piping(props: {
     components: ReactNode[]
 }) {
-    const [component, ...components] = props.components
+    const [component, ...components] = props.components;
 
     return <PipeProvider
         component={<Piping components={components} />}
         children={component}
-    />
+    />;
 }
